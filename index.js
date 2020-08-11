48e5a461cd16463c524037250139221d6302f88c
Emmanuel <akhigbemmanuel@gmail.com>
2020-08-11 19:42:31 +0100

    Use minimist to collect input

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const clear = require('clear')
 const chalk = require('chalk')
 const figlet = require('figlet')
+
+
+const files = require('./lib/files');
+const github = require('./lib/github');
+
+clear();
+
+console.log(
+    chalk.yellow(
+        figlet.textSync('vittli', { horizontalLayout: 'full' })
+    )
+)
+// if (files.hasDirectory('.git')) {
+//     console.log(chalk.red('Already a Git repository!'));
+//     process.exit();
+// }
+
+const run = async () => {
+    let token = github.getStoredGithubToken();
+    if (!token) {
+        token = await github.getPersonalAccessToken();
+    }
+    console.log(token);
+};
+
+run()
