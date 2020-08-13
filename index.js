45c187f5587546b36c303cb3f9de95ac1fa2b0e2
Emmanuel <akhigbemmanuel@gmail.com>
2020-08-13 23:10:26 +0100

    Add main logic and flow of application

--- index.js
+++ index.js
@@ -27,11 +27,35 @@ const getGithubToken = async () => {
 };
 
 const run = async () => {
-    let token = github.getStoredGithubToken();
-    if (!token) {
-        token = await github.getPersonalAccessToken();
+    try {
+        // Retrieve & Set Authentication Token
+        const token = await getGithubToken();
+        github.githubAuth(token);
+
+        // Create remote repository
+        const url = await repo.createRemoteRepo();
+
+        // Create .gitignore file
+        await repo.createGitignore();
+
+        // Set up local repository and push to remote
+        await repo.setupRepo(url);
+
+        console.log(chalk.green('All done!'));
+    } catch (err) {
+        if (err) {
+            switch (err.status) {
+                case 401:
+                    console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+                    break;
+                case 422:
+                    console.log(chalk.red('There is already a remote repository or token with the same name'));
+                    break;
+                default:
+                    console.log(chalk.red(err));
+            }
+        }
     }
-    console.log(token);
 };
 
 run()
